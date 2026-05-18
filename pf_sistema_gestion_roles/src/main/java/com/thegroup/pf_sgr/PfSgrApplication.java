e1a5be35e895ee95afa45986ba787a1de312d023
Santiago <santipenguin777@gmail.com>
2026-05-17 21:15:11 -0500

    feat: Configuración Inicial

---
+++ pf_sistema_gestion_roles/src/main/java/com/thegroup/pf_sgr/PfSgrApplication.java
@@ -0,0 +1,13 @@
+package com.thegroup.pf_sgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfSgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfSgrApplication.class, args);
+	}
+
+}
---
+++ pf_sistema_gestion_roles/src/test/java/com/thegroup/pf_sgr/PfSgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegroup.pf_sgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfSgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
