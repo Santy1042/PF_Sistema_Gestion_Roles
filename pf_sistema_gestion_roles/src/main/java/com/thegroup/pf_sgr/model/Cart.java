a87deb32f4b893918a1d8b2eb9715cb98408b8ef
Santiago <santipenguin777@gmail.com>
2026-05-25 12:00:23 -0500

    feat: Cart & CartItem Model
    Se crean los modelos para el carrito y los items que va a tener el carrito

---
+++ pf_sistema_gestion_roles/src/main/java/com/thegroup/pf_sgr/model/Cart.java
@@ -0,0 +1,33 @@
+package com.thegroup.pf_sgr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Long cartId;
+
+    @Column(name = "session_token")
+    private String sessionToken;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items;
+}
---
+++ pf_sistema_gestion_roles/src/main/java/com/thegroup/pf_sgr/model/CartItem.java
@@ -0,0 +1,30 @@
+package com.thegroup.pf_sgr.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_cart_id")
+    private Long itemCartId;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_variant_id", nullable = false)
+    private ProductVariant productVariant;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
