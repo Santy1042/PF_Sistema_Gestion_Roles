d8eb52178b505b30d2973e86dc144ac8d4fa5ed0
Santy S. <santipenguin777@gmail.com>
2026-06-03 12:54:26 -0500

    Update config.js

--- IU/js/config.js
+++ IU/js/config.js
@@ -1,2 +1,7 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
+const API_BASE_URL = isLocalhost 
+    ? 'http://localhost:8080/api' 
+    : 'https://pf-backend-api-7cwj.onrender.com';
+
 const STORAGE_KEY   = 'localCart';
